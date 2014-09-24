3aa114bd6aaf84852164c9c810544fff3fcb1d68
Alex Shishkin <roboswarm@gmail.com>
2014-09-24 08:57:52 +0400

    добавлены окна

--- www/js/script.js
+++ www/js/script.js
@@ -1,7 +1,31 @@
 var i7e = {
 	init: function() {
+		$('#news a').on('tap', function() {
+			$('#news').hide();
+			$($(this).attr('href')).show();
+		});
 		//$('#main_menu').
-		console.log(11);
+		/*
+
+		$('#seminars a').on('tap', function() {
+			$('#seminars').hide();
+			$($(this).attr('href')).show();
+		});
+		*/
+		$('#news').show();
+		$('#media #video').show();
+		$('#main_menu a').on('tap', function() {
+			$('div.main div.ui-content').hide();
+			console.log($(this).attr('href'));
+			$($(this).attr('href')).show();
+		});
+/*
+		$('div.main div.ui-content a').on('tap', function() {
+			$(this).closest('div.ui-content').hide();
+			$($(this).attr('href')).show();
+		});
+
+		*/
 	}
 }
 $(i7e.init);
